e70c56234f5c4e3cae2dd36a646417b0cfbcc3f5
1Unknown6 <djordjevicv.it@gmail.com>
2023-08-25 18:44:30 +0200

    state experiment 2

--- src/App.js
+++ src/App.js
@@ -19,11 +19,11 @@ const initialState = {
   route: 'signin',
   isSignedIn: false,
   user: {
-    id: '',
-    name: '',
-    email: '',
+    id: ' ',
+    name: ' ',
+    email: ' ',
     entries: 0,
-    joined: ''
+    joined: ' '
   },
   isModalOpen: false,
   topFive: [
