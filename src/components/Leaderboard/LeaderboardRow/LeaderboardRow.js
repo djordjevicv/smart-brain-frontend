698473c830ef78f04a6f42c3e223813f4e36578e
1Unknown6 <djordjevicv.it@gmail.com>
2023-08-26 13:08:28 +0200

    Limited the length of name in Leaderboard

--- src/components/Leaderboard/LeaderboardRow/LeaderboardRow.js
+++ src/components/Leaderboard/LeaderboardRow/LeaderboardRow.js
@@ -17,10 +17,12 @@ class LeaderboardRow extends React.Component{
                 </div>
             );
         }
+        const newName = Name.trim().substring(0, 7);
+
         return (
             <div id="LeaderboardRow" className="flex justify-around shadow-2 f6 f5-ns w-80 pa3 mv2 b">
                 <div id="rank" className="center">{`${Rank}`}</div>
-                <div id="name" className="center">{`${Name}`}</div>
+                <div id="name" className="center">{`${newName}`}</div>
                 <div id="entries" className="center">{`${Entries}`}</div>
             </div>
         );
