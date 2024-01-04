c80837dac267eca975df3ab3e144aa2e866713a5
1Unknown6 <djordjevicv.it@gmail.com>
2024-01-04 12:08:10 +0100

    implemented context api

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,19 +2,12 @@ import Rank from "../Rank/Rank"
 import LeaderBoard from "../Leaderboard/Leaderboard"
 import PictureScanner from "../PictureScanner/PictureScanner"
 
-export function Home({ user, changeOpen, isModalOpen, updateUserCount }) {
+export function Home() {
     return (
         <div>
-            <Rank
-                name={user.name}
-                entries={user.entries}
-            />
-            <LeaderBoard changeOpen={changeOpen}
-                isModalOpen={isModalOpen}
-            />
-            <PictureScanner updateUserCount={updateUserCount}
-                user={user}
-            />
+            <Rank />
+            <LeaderBoard />
+            <PictureScanner />
         </div>
     );
 }
